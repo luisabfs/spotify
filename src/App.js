226dd94f61169e1874cd6622be6a10cc73568d59
Luísa Barros <qlullyq@gmail.com>
2019-07-31 17:58:07 -0300

    chore: add global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  height, body, #root {
+    height: 100%;
+  }
+
+  body {
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    background: #181818;
+  }
+`;
+
+export default GlobalStyle;
