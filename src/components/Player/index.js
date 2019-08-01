cef94f4f1ceb34734c4b0a6bdca2966ab991d388
Luísa Barros <qlullyq@gmail.com>
2019-08-01 16:31:49 -0300

    feat: add player component

---
+++ src/components/Player/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Player = () => (
+  <Container>
+    <h1>Player</h1>
+  </Container>
+);
+
+export default Player;
---
+++ src/components/Player/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #282828;
+  padding: 12px;
+  height: 72px;
+`;
