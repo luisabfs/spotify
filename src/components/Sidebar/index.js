cc41f24532210d84bbdc8b30ff062920792dde94
Luísa Barros <qlullyq@gmail.com>
2019-07-31 22:23:25 -0300

    feat: add menus to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 
-import { Container, NewPlaylist } from './styles';
+import { Container, NewPlaylist, Nav } from './styles';
 
 import AddPlaylistIcon from '../../assets/images/add_playlist.svg';
 
 const Sidebar = () => (
   <Container>
-    <div />
+    <div>
+      <Nav main>
+        <li>
+          <a href="">Browse</a>
+        </li>
+        <li>
+          <a href="">Radio</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>YOUR LIBRARY</span>
+        </li>
+        <li>
+          <a href="">Your Daily Mix</a>
+        </li>
+        <li>
+          <a href="">Recently played</a>
+        </li>
+        <li>
+          <a href="">Songs</a>
+        </li>
+        <li>
+          <a href="">Albums</a>
+        </li>
+        <li>
+          <a href="">Artists</a>
+        </li>
+        <li>
+          <a href="">Stations</a>
+        </li>
+        <li>
+          <a href="">Videos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>PLAYLISTS</span>
+        </li>
+        <li>
+          <a href="">Focusing</a>
+        </li>
+        <li>
+          <a href="">Having fun!</a>
+        </li>
+      </Nav>
+    </div>
+
     <NewPlaylist>
       <img src={AddPlaylistIcon} alt="Add playlist" />
       New playlist
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -9,6 +9,41 @@ export const Container = styled.aside`
   width: 200px;
   background: #121212;
   color: #b3b3b3;
+
+  > div {
+    padding: 25px;
+  }
+`;
+
+export const Nav = styled.ul`
+  list-style: none;
+  margin-top: 25px;
+
+  &:first-child {
+    margin: 0;
+  }
+
+  li {
+    a {
+      color: inherit;
+      text-decoration: none;
+      font-size: 13px;
+      line-height: 32px;
+      font-weight: ${props => (props.main ? 'bold' : 'normal')};
+
+      &:hover {
+        color: #fff;
+      }
+    }
+
+    span {
+      font-size: 11px;
+      text-transform: uppercase;
+      line-height: 22px;
+      letter-spacing: 1.11px;
+      font-weight: 300;
+    }
+  }
 `;
 
 export const NewPlaylist = styled.button`
