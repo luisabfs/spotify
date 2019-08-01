19ad0dbcacfe6bed95b9ce1f5345800ac48c557c
Luísa Barros <qlullyq@gmail.com>
2019-08-01 16:33:39 -0300

    styles: add components style

---
+++ src/styles/components.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+`;
