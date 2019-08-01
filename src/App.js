c0a067d151983d3ad6ec172c6c7680fa55fef527
Luísa Barros <qlullyq@gmail.com>
2019-08-01 16:34:15 -0300

    feat: update App.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import { Wrapper, Container } from './styles/components';
 
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
 
-function App() {
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <Sidebar />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <GlobalStyle />
+    <Wrapper>
+      <Container>
+        <Sidebar />
+      </Container>
+      <Player />
+    </Wrapper>
+  </div>
+);
 
 export default App;
