382936311ab1fa70d18da49eee96fc5de169f502
Luísa Barros <qlullyq@gmail.com>
2019-08-01 23:22:07 -0300

    chore: add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import { Wrapper, Container, Content } from './styles/components';
@@ -7,19 +8,22 @@ import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Player from './components/Player';
 
+import Routes from './routes';
+
 const App = () => (
-  <div className="App">
+  <BrowserRouter>
     <GlobalStyle />
     <Wrapper>
       <Container>
         <Sidebar />
         <Content>
           <Header />
+          <Routes />
         </Content>
       </Container>
       <Player />
     </Wrapper>
-  </div>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/pages/browse/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Browse = () => <h1>Olar</h1>
+
+export default Browse;
