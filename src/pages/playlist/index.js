3b0f4a4c4d8171d849a3b2a5fe1ef446f9ee6b40
Luísa Barros <qlullyq@gmail.com>
2019-08-06 13:55:15 -0300

    styles: style playlist header

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const Playlist = () => <h1>Oi</h1>;
+import { Container, Header } from './styles';
+
+const Playlist = () => (
+  <Container>
+    <Header>
+      <img src="https://i.redd.it/3z8xam8wpo9y.png" alt="Playlist" />
+
+      <div>
+        <span>PLAYLIST</span>
+        <h1>Having fun!</h1>
+        <p>13 musics</p>
+
+        <button>PLAY</button>
+      </div>
+    </Header>
+  </Container>
+);
 
 export default Playlist;
