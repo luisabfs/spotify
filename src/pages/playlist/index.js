6e5917b0ad338bd82fb8c69e714a17fa78de05cf
Luísa Barros <qlullyq@gmail.com>
2019-08-06 14:08:33 -0300

    styles: style playlist table

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import { Container, Header } from './styles';
+import { Container, Header, SongList } from './styles';
+
+import ClockIcon from '../../assets/images/clock.svg';
+import PlusIcon from '../../assets/images/plus.svg';
 
 const Playlist = () => (
   <Container>
@@ -15,6 +18,75 @@ const Playlist = () => (
         <button>PLAY</button>
       </div>
     </Header>
+
+    <SongList cellPadding={0} cellSpacing={0}>
+      <thead>
+        <th />
+        <th>Title</th>
+        <th>Artist</th>
+        <th>Album</th>
+        <th>
+          <img src={ClockIcon} alt="Duration" />
+        </th>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Stressed Out</td>
+          <td>Twenty One Pilots</td>
+          <td>Blurryface</td>
+          <td>3:22</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Stressed Out</td>
+          <td>Twenty One Pilots</td>
+          <td>Blurryface</td>
+          <td>3:22</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Stressed Out</td>
+          <td>Twenty One Pilots</td>
+          <td>Blurryface</td>
+          <td>3:22</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Stressed Out</td>
+          <td>Twenty One Pilots</td>
+          <td>Blurryface</td>
+          <td>3:22</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Stressed Out</td>
+          <td>Twenty One Pilots</td>
+          <td>Blurryface</td>
+          <td>3:22</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Add" />
+          </td>
+          <td>Stressed Out</td>
+          <td>Twenty One Pilots</td>
+          <td>Blurryface</td>
+          <td>3:22</td>
+        </tr>
+      </tbody>
+    </SongList>
   </Container>
 );
 
--- src/pages/playlist/styles.js
+++ src/pages/playlist/styles.js
@@ -49,3 +49,42 @@ export const Header = styled.header`
     }
   }
 `;
+
+export const SongList = styled.table`
+  width: 100%;
+  text-align: left;
+  margin-top: 20px;
+
+  thead th {
+    font-size: 11px;
+    color: #b3b3b3;
+    letter-spacing: 1.11px;
+    font-weight: normal;
+    text-transform: uppercase;
+    padding: 5px 10px;
+
+    &:last-child {
+      text-align: right;
+    }
+  }
+
+  tbody td {
+    border-top: 1px solid #282828;
+    font-size: 13px;
+    padding: 0 10px;
+    line-height: 40px;
+
+    &:first-child {
+      width: 80px;
+      text-align: right;
+    }
+
+    &:last-child {
+      text-align: right;
+    }
+  }
+
+  tbody tr:hover td {
+    background: #282828;
+  }
+`;
