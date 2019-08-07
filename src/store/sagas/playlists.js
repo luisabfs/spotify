974f5a91a309cd09dd033dc516ba17a5f65c9ae5
Luísa Barros <qlullyq@gmail.com>
2019-08-06 23:20:39 -0300

    feat(saga): get data from api

--- src/store/sagas/playlists.js
+++ src/store/sagas/playlists.js
@@ -7,7 +7,7 @@ export function* getPlaylists() {
   try {
     const response = yield call(api.get, '/playlists');
 
-    yield put(PlaylistsActions.getPlaylistSuccess(response.data));
+    yield put(PlaylistsActions.getPlaylistsSuccess(response.data));
   } catch (err) {
     console.log(err);
   }
