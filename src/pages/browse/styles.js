95c5954e03277be087cc3809052e94f984673ae6
Luísa Barros <qlullyq@gmail.com>
2019-08-09 11:54:05 -0300

    styles: style loading component on browse page

--- src/pages/browse/styles.js
+++ src/pages/browse/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+import { Spinner } from '../../components/Loading/styles';
+
 export const Container = styled.div`
   display: flex;
   flex: 1;
@@ -10,6 +12,10 @@ export const Container = styled.div`
 
 export const Title = styled.h1`
   font-size: 48px;
+
+  ${Spinner} {
+    height: 24px;
+  }
 `;
 
 export const List = styled.div`
